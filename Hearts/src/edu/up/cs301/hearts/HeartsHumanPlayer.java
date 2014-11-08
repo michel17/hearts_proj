402a7d687082d8ab2e15400bc330e058bc65407d
Sharkbuck <michel17@up.edu>
2014-11-08 14:20:27 -0800

    App now starts. Kind of.

--- Hearts/src/edu/up/cs301/hearts/HeartsHumanPlayer.java
+++ Hearts/src/edu/up/cs301/hearts/HeartsHumanPlayer.java
@@ -1,30 +1,68 @@
 package edu.up.cs301.hearts;
 
+import android.app.Activity;
 import android.graphics.Canvas;
 import android.view.MotionEvent;
 import android.view.View;
+import edu.up.cs301.animation.AnimationSurface;
 import edu.up.cs301.animation.Animator;
+import edu.up.cs301.card.Card;
 import edu.up.cs301.game.GameHumanPlayer;
 import edu.up.cs301.game.GameMainActivity;
+import edu.up.cs301.game.R;
 import edu.up.cs301.game.infoMsg.GameInfo;
 
 public class HeartsHumanPlayer extends GameHumanPlayer implements Animator {
+	// sizes and locations of card decks and cards, expressed as percentages
+		// of the screen height and width
+		private final static float CARD_HEIGHT_PERCENT = 50; // height of a card
+		private final static float CARD_WIDTH_PERCENT = 17; // width of a card
+		private final static float LEFT_BORDER_PERCENT = 4; // width of left border
+		private final static float RIGHT_BORDER_PERCENT = 20; // width of right border
+		private final static float VERTICAL_BORDER_PERCENT = 4; // width of top/bottom borders
+		
+		// our game state
+		protected HeartsState state;
+
+		// our activity
+		private Activity myActivity;
+
+		// the animation surface
+		private AnimationSurface surface;
+		
+		// the background color
+		private int backgroundColor;
 
 	public HeartsHumanPlayer(String name) {
 		super(name);
+		backgroundColor = 0xff006400;
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void setAsGui(GameMainActivity activity) {
-		// TODO Auto-generated method stub
-		
+		myActivity = activity;
+		// Load the layout resource for the new configuration
+				activity.setContentView(R.layout.hearts_human_player);
+
+				// link the animator (this object) to the animation surface
+				surface = (AnimationSurface) myActivity
+						.findViewById(R.id.animation_surface);
+				surface.setAnimator(this);
+				
+				// read in the card images
+				Card.initImages(activity);
+
+				// if the state is not null, simulate having just received the state so that
+				// any state-related processing is done
+				if (state != null) {
+					receiveInfo(state);
+				}
 	}
 
 	@Override
 	public View getTopView() {
-		// TODO Auto-generated method stub
-		return null;
+		return myActivity.findViewById(R.id.top_gui_layout);
 	}
 
 	@Override
@@ -36,13 +74,13 @@ public class HeartsHumanPlayer extends GameHumanPlayer implements Animator {
 	@Override
 	public int interval() {
 		// TODO Auto-generated method stub
-		return 0;
+		return 20;
 	}
 
 	@Override
 	public int backgroundColor() {
 		// TODO Auto-generated method stub
-		return 0;
+		return backgroundColor;
 	}
 
 	@Override
--- Hearts/src/edu/up/cs301/hearts/HeartsMainActivity.java
+++ Hearts/src/edu/up/cs301/hearts/HeartsMainActivity.java
@@ -1,21 +1,47 @@
 package edu.up.cs301.hearts;
 
+import java.util.ArrayList;
+
+import android.graphics.Color;
 import edu.up.cs301.game.GameMainActivity;
+import edu.up.cs301.game.GamePlayer;
 import edu.up.cs301.game.LocalGame;
 import edu.up.cs301.game.config.GameConfig;
+import edu.up.cs301.game.config.GamePlayerType;
 
 public class HeartsMainActivity extends GameMainActivity {
+	public static final int PORT_NUMBER = 4752;
 
 	@Override
 	public GameConfig createDefaultConfig() {
-		
-		return null;
+		// Define the allowed player types
+		ArrayList<GamePlayerType> playerTypes = new ArrayList<GamePlayerType>();
+
+		playerTypes.add(new GamePlayerType("human player") {
+			public GamePlayer createPlayer(String name) {
+				return new HeartsHumanPlayer(name);
+			}
+		});
+		// Create a game configuration class for SlapJack
+		GameConfig defaultConfig = new GameConfig(playerTypes, 2, 2, "Hearts",
+				PORT_NUMBER);
+
+		// Add the default players
+		defaultConfig.addPlayer("Human", 0);
+		defaultConfig.addPlayer("Human", 0);
+
+		// Set the initial information for the remote player
+		defaultConfig.setRemoteData("Guest", "", 0);
+
+		// done!
+		return defaultConfig;
+
 	}
 
 	@Override
 	public LocalGame createLocalGame() {
-		
-		return null;
+
+		return new HeartsLocalGame();
 	}
 
 }
