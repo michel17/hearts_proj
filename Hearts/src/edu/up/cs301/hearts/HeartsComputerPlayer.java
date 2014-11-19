f3581ecb9616df3f4e779edf369582442e0ba1cf
stevenlindhb <linds17@up.edu>
2014-11-19 08:13:01 -0800

    This might kill it
    Weird deleted stuff in the changed files list

--- Hearts/src/edu/up/cs301/hearts/HeartsComputerPlayer.java
+++ Hearts/src/edu/up/cs301/hearts/HeartsComputerPlayer.java
@@ -1,9 +1,18 @@
 package edu.up.cs301.hearts;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import android.util.Log;
+import edu.up.cs301.card.Card;
+import edu.up.cs301.card.Rank;
+import edu.up.cs301.card.Suit;
 import edu.up.cs301.game.GameComputerPlayer;
 import edu.up.cs301.game.infoMsg.GameInfo;
 
 public class HeartsComputerPlayer extends GameComputerPlayer {
+	HeartsState state;
+	ArrayList<Card> hand = new ArrayList<>();
 
 	public HeartsComputerPlayer(String name) {
 		super(name);
@@ -12,20 +21,92 @@ public class HeartsComputerPlayer extends GameComputerPlayer {
 
 	@Override
 	protected void receiveInfo(GameInfo info) {
-		// TODO Auto-generated method stub
-		
+		System.out.println(info.toString());
+		Log.i("HeartsComputerPlayer", "receiving updated state (" + info.getClass() + ")");
+		if (!(info instanceof HeartsState)) {
+			// otherwise, if it's not a game-state message, ignore
+			return;
+		} else {
+			// it's a game-state object: update the state. Since we have an
+			// animation
+			// going, there is no need to explicitly display anything. That will
+			// happen
+			// at the next animation-tick, which should occur within 1/20 of a
+			// second
+			this.state = (HeartsState) info;
+			hand = state.getPlayerHand(playerNum);
+			Log.i("Computer player", "receiving");
+		}
+
 	}
+
 	@Override
 	protected void timerTicked() {
-		if(game != null && game instanceof HeartsLocalGame){
-			
-			if(((HeartsLocalGame)game).canMove(playerNum)){
-				//Make a move
+		if (game != null && game instanceof HeartsLocalGame) {
+
+		}
+		Card[] trick = state.getCurrentTrick();
+		Suit leadSuit = null;
+		if (trick[0] != null) {
+			leadSuit = trick[0].getSuit();
+		} else {
+			ArrayList<Card> whiteList = new ArrayList<Card>();
+			Collections.copy(whiteList, hand);
+			Card currentCard = whiteList.get((int) (Math.random() * hand.size()));
+			while (currentCard.getSuit() == Suit.Heart
+					|| (currentCard.getSuit() != leadSuit && leadSuit != null)) {
+				if (state.isHeartsBroken()) {
+					break;
+				} else {
+					whiteList.remove(currentCard);
+					if (leadSuit == null) {
+						currentCard = hand.get((int) (Math.random() * whiteList.size()));
+					} else {
+						Rank rank = Rank.ACE;
+						for (int i = 0; i < trick.length; i++) {
+							Rank tempRank = trick[i].getRank();
+							if (tempRank.value(14) < rank.value(14)) {
+								rank = tempRank;
+							}
+						}
+						Rank lowestRank = Rank.ACE;
+						Card lowestRankCard = null;
+						for (int i = 0; i < whiteList.size(); i++) {
+							Rank tempRank = whiteList.get(i).getRank();
+							if (tempRank.value(14) < lowestRank.value(14)) {
+								lowestRank = tempRank;
+								lowestRankCard = whiteList.get(i);
+							}
+						}
+						if (lowestRank.value(14) < rank.value(14)) {
+							ArrayList<Card> tempCards = new ArrayList<Card>();
+							for (int i = 0; i < whiteList.size(); i++) {
+								if (whiteList.get(i).getRank().value(14) < rank.value(14)) {
+									tempCards.add(whiteList.get(i));
+								}
+							}
+							Rank highestRank = Rank.TWO;
+							Card highestRankCard = null;
+							for (int i = 0; i < tempCards.size(); i++) {
+								if (tempCards.get(i).getRank().value(14) > highestRank.value(14)) {
+									highestRank = tempCards.get(i).getRank();
+									highestRankCard = tempCards.get(i);
+								}
+							}
+							currentCard = highestRankCard;
+						} else {
+							currentCard = lowestRankCard;
+						}
+
+					}
+				}
 			}
+			game.sendAction(new HeartsPlayAction(this, currentCard));
+			hand.remove(currentCard);
 		}
 	}
-	
-	public int getPlayerNumber(){
+
+	public int getPlayerNumber() {
 		return playerNum;
 	}
 
--- Hearts/src/edu/up/cs301/hearts/HeartsHumanPlayer.java
+++ Hearts/src/edu/up/cs301/hearts/HeartsHumanPlayer.java
@@ -3,10 +3,12 @@ package edu.up.cs301.hearts;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Picture;
 import android.graphics.PointF;
 import android.graphics.Rect;
 import android.graphics.RectF;
@@ -41,9 +43,9 @@ public class HeartsHumanPlayer extends GameHumanPlayer implements Animator {
 	private int backgroundColor;
 
 	private Paint paint;
-	private boolean hasChecked;
+	private boolean hasChecked, displayStart;
 	private char[] alphabet = { 'A', 'B', 'C', 'D' };
-	private int design = 0;
+	private int design = 0, alpha = 200;
 	private Path wallPath;
 	private ArrayList<PointF> scorePoint = new ArrayList<>(3);
 	private static float width, height;
@@ -68,8 +70,7 @@ public class HeartsHumanPlayer extends GameHumanPlayer implements Animator {
 		activity.setContentView(R.layout.hearts_human_player);
 
 		// link the animator (this object) to the animation surface
-		surface = (AnimationSurface) myActivity
-				.findViewById(R.id.animation_surface);
+		surface = (AnimationSurface) myActivity.findViewById(R.id.animation_surface);
 		surface.setAnimator(this);
 
 		// read in the card images
@@ -91,8 +92,7 @@ public class HeartsHumanPlayer extends GameHumanPlayer implements Animator {
 	@Override
 	public void receiveInfo(GameInfo info) {
 		System.out.println(info.toString());
-		Log.i("HeartsHumanPlayer",
-				"receiving updated state (" + info.getClass() + ")");
+		Log.i("HeartsHumanPlayer", "receiving updated state (" + info.getClass() + ")");
 		if (info instanceof IllegalMoveInfo || info instanceof NotYourTurnInfo) {
 			// if we had an out-of-turn or illegal move, flash the screen
 			surface.flash(Color.RED, 50);
@@ -137,43 +137,38 @@ public class HeartsHumanPlayer extends GameHumanPlayer implements Animator {
 		width = g.getWidth();
 		height = g.getHeight();
 
-		if (!hasChecked)
-			pointUpdate();
+		if (!hasChecked) pointUpdate();
 		paint.setColor(Color.WHITE);
 		paint.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD_ITALIC));
 		paint.setTextSize(20);
 		for (int i = 0; i < scorePoint.size(); i++) {
-			g.drawText("Score: 0", scorePoint.get(i).x, scorePoint.get(i).y,
-					paint);
+			g.drawText("Score: 0", scorePoint.get(i).x, scorePoint.get(i).y, paint);
 			if (i == 3) {
 				paint.setColor(Color.YELLOW);
 				paint.setTextSize(30);
-				g.drawText("YOU", scorePoint.get(i).x,
-						scorePoint.get(i).y - 30, paint);
+				g.drawText("Filthy Casual", scorePoint.get(i).x - 70, scorePoint.get(i).y - 30, paint);
 			} else {
-				g.drawText("Player " + String.valueOf(alphabet[i]),
-						scorePoint.get(i).x, scorePoint.get(i).y - 30, paint);
+				g.drawText("Player " + String.valueOf(alphabet[i]), scorePoint.get(i).x,
+						scorePoint.get(i).y - 30, paint);
 			}
 		}
 
 		Rect r = new Rect((int) width / 5, (int) ((height / 4) - (height / 8)),
-				(int) (width - width / 5),
-				(int) ((height - height / 4) - (height / 8)));
+				(int) (width - width / 5), (int) ((height - height / 4) - (height / 8)));
 		paint.setStyle(Paint.Style.FILL);
 		paint.setColor(0xff640000);
 		g.drawRect(r, paint);
 		paint.setColor(Color.WHITE);
 		paint.setTextSize(20);
-		g.drawText("Current Trick", (float) (width / 4.6),
-				(float) (height / 5.3), paint);
+		g.drawText("Current Trick", (float) (width / 4.6), (float) (height / 5.3), paint);
 		paint.setColor(Color.YELLOW);
 		paint.setTextSize(15);
 		g.drawText("01", (float) (width / 3.2), (float) (height / 5.9), paint);
 
 		paint.setStyle(Paint.Style.STROKE);
 		g.drawRect(r, paint);
-		r = new Rect((int) width / 5, (int) ((height / 4) - (height / 8)),
-				(int) (width / 3), (int) ((height / 3) - (height / 8)));
+		r = new Rect((int) width / 5, (int) ((height / 4) - (height / 8)), (int) (width / 3),
+				(int) ((height / 3) - (height / 8)));
 
 		g.drawRect(r, paint);
 		for (design = 0; design < 3; design++) {
@@ -183,6 +178,7 @@ public class HeartsHumanPlayer extends GameHumanPlayer implements Animator {
 		}
 		drawTrick(g);
 		drawCards(g);
+		
 	}
 
 	@Override
@@ -190,10 +186,8 @@ public class HeartsHumanPlayer extends GameHumanPlayer implements Animator {
 		if (event.getAction() == MotionEvent.ACTION_DOWN) {
 			float tx = event.getX();
 			float ty = event.getY();
-			Rect trickBoundingBox = new Rect((int) width / 5,
-					(int) ((height / 4) - (height / 8)),
-					(int) (width - width / 5),
-					(int) ((height - height / 4) - (height / 8)));
+			Rect trickBoundingBox = new Rect((int) width / 5, (int) ((height / 4) - (height / 8)),
+					(int) (width - width / 5), (int) ((height - height / 4) - (height / 8)));
 			if (selectedCard != null && trickBoundingBox.contains((int) tx, (int) ty)) {
 				// playing card
 				game.sendAction(new HeartsPlayAction(this, selectedCard));
@@ -201,9 +195,9 @@ public class HeartsHumanPlayer extends GameHumanPlayer implements Animator {
 			} else {
 				// Selection / deselection of cards
 				int handIdx = (int) ((hand.size()) * tx / width);
-				if (ty > height - (height / 3)
-						&& !(hand.get(handIdx) == selectedCard)) {// in bounds
-																	// vertically
+				if (ty > height - (height / 3) && !(hand.get(handIdx) == selectedCard)) {// in
+																							// bounds
+																							// vertically
 					selectedCard = hand.get(handIdx);
 				} else {
 					selectedCard = null;
@@ -231,28 +225,20 @@ public class HeartsHumanPlayer extends GameHumanPlayer implements Animator {
 		switch (design) {
 		case 0:
 			wallPath.moveTo(width / 5, height - height / 4 - (height / 8));
-			wallPath.lineTo(width / 5, height - height / 4 - triangleDepth
-					- (height / 8));
-			wallPath.lineTo(width / 5 + triangleDepth, height - height / 4
-					- (height / 8));
+			wallPath.lineTo(width / 5, height - height / 4 - triangleDepth - (height / 8));
+			wallPath.lineTo(width / 5 + triangleDepth, height - height / 4 - (height / 8));
 			wallPath.lineTo(width / 5, height - height / 4 - (height / 8));
 			break;
 		case 1:
-			wallPath.moveTo(width - width / 5, height - height / 4
-					- (height / 8));
-			wallPath.lineTo(width - width / 5, height - height / 4
-					- triangleDepth - (height / 8));
-			wallPath.lineTo(width - width / 5 - triangleDepth, height - height
-					/ 4 - (height / 8));
-			wallPath.lineTo(width - width / 5, height - height / 4
-					- (height / 8));
+			wallPath.moveTo(width - width / 5, height - height / 4 - (height / 8));
+			wallPath.lineTo(width - width / 5, height - height / 4 - triangleDepth - (height / 8));
+			wallPath.lineTo(width - width / 5 - triangleDepth, height - height / 4 - (height / 8));
+			wallPath.lineTo(width - width / 5, height - height / 4 - (height / 8));
 			break;
 		case 2:
 			wallPath.moveTo(width - width / 5, height / 4 - (height / 8));
-			wallPath.lineTo(width - width / 5, height / 4 + triangleDepth
-					- (height / 8));
-			wallPath.lineTo(width - width / 5 - triangleDepth, height / 4
-					- (height / 8));
+			wallPath.lineTo(width - width / 5, height / 4 + triangleDepth - (height / 8));
+			wallPath.lineTo(width - width / 5 - triangleDepth, height / 4 - (height / 8));
 			wallPath.lineTo(width - width / 5, height / 4 - (height / 8));
 			break;
 		}
@@ -264,9 +250,8 @@ public class HeartsHumanPlayer extends GameHumanPlayer implements Animator {
 			if (selectedCard != null && hand.get(i).equals(selectedCard)) {
 				drawSelectedCard(g, hand.get(i), i);
 			} else {
-				RectF r = new RectF((width / currentspacing) * i,
-						(height - (height / 3)), (width / currentspacing) * i
-								+ 150, height);
+				RectF r = new RectF((width / currentspacing) * i, (height - (height / 3)),
+						(width / currentspacing) * i + 150, height);
 				hand.get(i).drawOn(g, r);
 			}
 		}
@@ -278,12 +263,10 @@ public class HeartsHumanPlayer extends GameHumanPlayer implements Animator {
 		}
 		paint.setColor(Color.YELLOW);
 		RectF highlight = new RectF((width / currentspacing) * loc - 1,
-				(height - (height / 3)) - 21, (width / currentspacing) * loc
-						+ 151, height - 19);
+				(height - (height / 3)) - 21, (width / currentspacing) * loc + 151, height - 19);
 		g.drawRect(highlight, paint);
-		RectF r = new RectF((width / currentspacing) * loc,
-				(height - (height / 3)) - 20, (width / currentspacing) * loc
-						+ 150, height - 20);
+		RectF r = new RectF((width / currentspacing) * loc, (height - (height / 3)) - 20,
+				(width / currentspacing) * loc + 150, height - 20);
 		c.drawOn(g, r);
 	}
 
@@ -294,10 +277,9 @@ public class HeartsHumanPlayer extends GameHumanPlayer implements Animator {
 		Card[] trick = state.getCurrentTrick();
 		for (int i = 0; i < trick.length; i++) {
 			if (trick[i] != null) {
-				RectF r = new RectF((width / 5) + i * (width - (2 * width / 5))
-						/ 4 + 20, height / 4, (width / 5) + i
-						* (width - (2 * width / 5)) / 4 + 170, height
-						- (3 * height / 8) - 20);
+				RectF r = new RectF((width / 5) + i * (width - (2 * width / 5)) / 4 + 20,
+						height / 4, (width / 5) + i * (width - (2 * width / 5)) / 4 + 170, height
+								- (3 * height / 8) - 20);
 				trick[i].drawOn(g, r);
 			}
 		}
